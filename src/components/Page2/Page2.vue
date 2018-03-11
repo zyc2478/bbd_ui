e3187514834a5bd54a0d88a19c52088308fa9ee8
zyc2478 <zyc2478@126.com>
2018-03-12 00:06:48 +0800

    尝试加入前后端分离，获取数据用axios，但还未测试成功

--- src/components/Page2/Page2.vue
+++ src/components/Page2/Page2.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
+    <div id="app123">
+        <h1>服务端数据为：{{serverData}}</h1>
+        <img src="../../assets/logo.png" v-on:click="getData">
+        <router-view></router-view>
     </div>
 </template>
 
-<script type="text/ecmascript-6">
+<script>
     export default {
-        data() {
+        name: 'app123',
+        data () {
             return {
-
+                serverData: 'data from fake server'
+            }
+        },
+        mounted: function () {
+            this.getData()
+        },
+        methods: {
+            getData () {
+                console.log('-------getData')
+                var that = this
+                    //.$http.get('http://localhost:8080/BidAnalysis/hello/getUser/1')
+                //192.168.1.101为后端IP地址     this.$http.get('http://192.168.1.101:8080/hello/getperson/33333')
+                    .then(function (response) {
+                        console.log(response)
+                        console.log(this)
+                        that.serverData = response.data
+                    })
+                    .catch(function (error) {
+                        console.log(error)
+                    })
             }
         }
     }
